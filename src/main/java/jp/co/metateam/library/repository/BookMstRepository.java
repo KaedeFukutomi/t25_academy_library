65737afcc2e79c17b4190f03362e52e389249d5d
MT 福冨 楓 <kaede.fukutomi@metateam.co.jp>
2025-05-21 20:01:18 +0900

    物理削除機能のコードの修正

--- src/main/java/jp/co/metateam/library/repository/BookMstRepository.java
+++ src/main/java/jp/co/metateam/library/repository/BookMstRepository.java
@@ -14,9 +14,6 @@ public interface BookMstRepository extends JpaRepository<BookMst, Long> {
     @Query(value = "SELECT * FROM book_mst LIMIT 1000", nativeQuery = true)
     List<BookMst> findLimitedBook();
 
-    @Query(value = "SELECT * FROM book_mst WHERE id = ?1", nativeQuery = true)
-    Optional<BookMst> selectById(Long id);
-
     @Query(value = "SELECT * FROM book_mst WHERE isbn = ?1 AND deleted = false", nativeQuery = true)
     Optional<BookMst> selectByIsbn(String isbn);
 
